45ae305acc734784c8ba4dc3053af38786d07edb
thinker3197 <ashishgupta.3197@gmail.com>
2016-09-25 03:54:28 +0530

    Update lib structure + Add custom scroll listeners

--- src/progressively.js
+++ src/progressively.js
@@ -7,34 +7,27 @@
  */
 
 ;
-(function(window) {
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(function() {
+            return factory(root);
+        });
+    } else if (typeof exports === 'object') {
+        module.exports = factory;
+    } else {
+        root.progressively = factory(root);
+    }
+})(this, function(root) {
+
     'use strict';
 
-    var init,
-        extend,
-        anim,
-        loadImg,
-        parseElements,
-        defaults, settings = {},
-        pe;
+    var progressively = {};
 
-    /*
-     * default settings
-     */
+    var defaults, callback, poll, useDebounce;
 
-    defaults = {
-        async: true,
-        blur: 20,
-        delay: 12 // better to keep it low
-    };
+    callback = function() {};
 
-    /*
-     * function to extend to objects
-     * @params primaryObject object, secondaryObject object
-     * @return object
-     */
-
-    extend = function et(primaryObject, secondaryObject) {
+    function extend(primaryObject, secondaryObject) {
         var o = {};
         for (var prop in primaryObject) {
             o[prop] = secondaryObject.hasOwnProperty(prop) ? secondaryObject[prop] : primaryObject[prop];
@@ -42,116 +35,93 @@
         return o;
     };
 
-    /*
-     * function to load images
-     * @params el HTMLElement
-     * @returns
-     */
-
-    loadImg = function li(el) {
-        /*
-         * set primary background before any image loads
-         */
-
-        el.style.background = '#efefef';
-
-        function load() {
-            var progressiveImg = new Image(),
-                originalImg = new Image();
-
-            progressiveImg.onload = function() {
-                el.src = this.src;
-            };
-            originalImg.onload = function() {
-                el.src = this.src;
-            };
-
-            progressiveImg.src = el.dataset.progressive;
-            originalImg.src = el.dataset.original;
-        }
-
-        if (settings.async) {
-
-            /*
-             * load images asynchronously
-             */
-
-            setTimeout(function() {
-                load();
-            }, settings.delay);
-
-        } else {
-
-            /*
-             * load images synchronously
-             */
-
-            load();
-        }
-
-        return;
+    function isHidden(el) {
+        return (el.offsetParent === null);
     };
 
+    function inView(el, view) {
+        if (isHidden(el)) {
+            return false;
+        }
+
+        var box = el.getBoundingClientRect();
+        return (
+            box.top >= 0 &&
+            box.left >= 0 &&
+            box.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            box.right <= (window.innerWidth || document.documentElement.clientWidth));
+
+    };
+
+    function loadImg(el, callback) {
+        setTimeout(function() {
+            var img = new Image();
+
+            img.onload = function() {
+                el.src = this.src;
+                if (typeof callback === 'function')
+                    callback();
+            };
+
+            img.src = el.dataset.progressive;
+        }, defaults.delay);
+    };
+
+    function listen() {
+        if (!useDebounce && !!poll) {
+            return;
+        }
+        clearTimeout(poll);
+        poll = setTimeout(function() {
+            progressively.render();
+            poll = null;
+        }, defaults.throttle);
+    }
     /*
-     * function to parse HTMLElements
-     * @params elems HTMLElement classifier
-     * @returns Array parsedElems
+     * default settings
      */
 
-    parseElements = function pe(elems) {
-        var parsedElems = [];
-        if (typeof elems === 'string') {
-            parsedElems.push(document.querySelectorAll(elems));
-        } else if (Array.isArray(elems)) {
-            for (var i = elems.length - 1; i >= 0; --i) {
-                parsedElems.push(document.querySelectorAll(elems[i]));
-            }
-        } else throw TypeError('Undefined format!');
-    }
+    defaults = {
+        throttle: 250,
+        blur: 20,
+        delay: 12
+    };
 
-    init = function(elems, options) {
-        elems = elems || [];
+    progressively.init = function(options) {
         options = options || {};
 
-        /*
-         * Extend default settings with user options
-         */
+        defaults = extend(defaults, options);
 
-        settings = extend.call(undefined, defaults, options);
+        progressively.render();
 
-
-        /*
-         * Parse HTMLElements and attach scroll listeners to them
-         */
-
-        listen(parsedElems.call(undefined, elems));
-
-        return;
+        if (document.addEventListener) {
+            root.addEventListener('scroll', listen, false);
+            root.addEventListener('load', listen, false);
+        } else {
+            root.attachEvent('onscroll', listen);
+            root.attachEvent('onload', listen);
+        }
     };
 
-    /*
-     * Exposure when used in AMD environment
-     */
+    progressively.render = function() {
+        var fnodes = document.querySelectorAll('.progressive-img'),
+            inodes = document.querySelectorAll('[data-progressive]'),
+            elem;
 
-    if (typeof define === 'function' && define.amd) {
-        define(function() {
-            return init;
-        });
-        return;
-    }
+        for (var i = inodes.length - 1; i >= 0; --i) {
+            elem = inodes[i];
 
-    /*
-     * Exposure when being used with NodeJS
-     */
+            if (inView(elem))
+                loadImg(elem);
 
-    if ('undefined' !== typeof module && module.exports) {
-        module.exports = init;
-        return;
-    }
+        }
 
-    /*
-     * Exposure to the window, if it exists
-     */
+        if (!inodes.length) {
+            progressively.drop();
+        }
+    };
 
-    window.progressively = init;
-})(('undefined' !== typeof window ? window : {}));
+    progressively.drop = function() {};
+
+    return progressively;
+});
