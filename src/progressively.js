6bc4af3fba0af326da8bba6ff1b1fb61db21172b
Thiago Santos <thia.mdossantos@gmail.com>
2017-03-05 11:22:52 -0300

    make js file load css and attach them to head

--- src/progressively.js
+++ src/progressively.js
@@ -6,141 +6,128 @@
  * Copyright (C) 2016 Ashish
  */
 
-;
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(function () {
-      return factory(root)
-    })
-  } else if (typeof exports === 'object') {
-    module.exports = factory
-  } else {
-    root.progressively = factory(root)
+import './progressively.css'
+
+var progressively = {}
+
+var defaults, poll, onLoad, inodes
+
+onLoad = function () {}
+
+function extend (primaryObject, secondaryObject) {
+  var o = {}
+  for (var prop in primaryObject) {
+    o[prop] = secondaryObject.hasOwnProperty(prop) ? secondaryObject[prop] : primaryObject[prop]
   }
-})(this, function (root) {
-  'use strict'
+  return o
+};
 
-  var progressively = {}
+function isHidden (el) {
+  return (el.offsetParent === null)
+};
 
-  var defaults, poll, onLoad, inodes
-
-  onLoad = function () {}
-
-  function extend (primaryObject, secondaryObject) {
-    var o = {}
-    for (var prop in primaryObject) {
-      o[prop] = secondaryObject.hasOwnProperty(prop) ? secondaryObject[prop] : primaryObject[prop]
-    }
-    return o
-  };
-
-  function isHidden (el) {
-    return (el.offsetParent === null)
-  };
-
-  function inView (el, view) {
-    if (isHidden(el)) {
-      return false
-    }
-
-    var box = el.getBoundingClientRect()
-    return (
-            box.top >= 0 &&
-            box.left >= 0 &&
-            box.right <= (window.innerWidth || document.el.clientWidth) &&
-            box.bottom <= (window.innerHeight || document.el.clientHeight) ||
-            el.clientHeight >= window.innerHeight
-    )
-  };
-
-  function loadImage (el) {
-    setTimeout(function () {
-      var img = new Image()
-
-      img.onload = function () {
-        el.classList.remove('progressive--not-loaded')
-        el.classList.add('progressive--is-loaded')
-        el.src = this.src
-
-        onLoad(el)
-      }
-
-      img.src = el.dataset.progressive
-    }, defaults.delay)
-  };
-
-  function listen () {
-    if (poll) {
-      return
-    }
-    clearTimeout(poll)
-    poll = setTimeout(function () {
-      progressively.check()
-      progressively.render()
-      poll = null
-    }, defaults.throttle)
-  }
-    /*
-     * default settings
-     */
-
-  defaults = {
-    throttle: 300, // appropriate value, don't change unless intended
-    delay: 100,
-    onLoadComplete: function () {},
-    onLoad: function () {}
+function inView (el, view) {
+  if (isHidden(el)) {
+    return false
   }
 
-  progressively.init = function (options) {
-    options = options || {}
+  var box = el.getBoundingClientRect()
+  return (
+          box.top >= 0 &&
+          box.left >= 0 &&
+          box.right <= (window.innerWidth || document.el.clientWidth) &&
+          box.bottom <= (window.innerHeight || document.el.clientHeight) ||
+          el.clientHeight >= window.innerHeight
+  )
+};
 
-    defaults = extend(defaults, options)
+function loadImage (el) {
+  setTimeout(function () {
+    var img = new Image()
 
-    onLoad = defaults.onLoad || onLoad
+    img.onload = function () {
+      el.classList.remove('progressive--not-loaded')
+      el.classList.add('progressive--is-loaded')
+      el.src = this.src
 
-    inodes = [].slice.call(document.querySelectorAll('.progressive__img'))
+      onLoad(el)
+    }
 
+    img.src = el.dataset.progressive
+  }, defaults.delay)
+};
+
+function listen () {
+  if (poll) {
+    return
+  }
+  clearTimeout(poll)
+  poll = setTimeout(function () {
+    progressively.check()
     progressively.render()
+    poll = null
+  }, defaults.throttle)
+}
+  /*
+   * default settings
+   */
 
-    if (document.addEventListener) {
-      root.addEventListener('scroll', listen, false)
-      root.addEventListener('load', listen, false)
-    } else {
-      root.attachEvent('onscroll', listen)
-      root.attachEvent('onload', listen)
+defaults = {
+  throttle: 300, // appropriate value, don't change unless intended
+  delay: 100,
+  onLoadComplete: function () {},
+  onLoad: function () {}
+}
+
+progressively.init = function (options) {
+  options = options || {}
+
+  defaults = extend(defaults, options)
+
+  onLoad = defaults.onLoad || onLoad
+
+  inodes = [].slice.call(document.querySelectorAll('.progressive__img'))
+
+  progressively.render()
+
+  if (document.addEventListener) {
+    window.addEventListener('scroll', listen, false)
+    window.addEventListener('load', listen, false)
+  } else {
+    window.attachEvent('onscroll', listen)
+    window.attachEvent('onload', listen)
+  }
+}
+
+progressively.render = function () {
+  var elem
+
+  for (var i = inodes.length - 1; i >= 0; --i) {
+    elem = inodes[i]
+
+    if (inView(elem) && elem.classList.contains('progressive--not-loaded')) {
+      loadImage(elem)
+      inodes.splice(i, 1)
     }
   }
 
-  progressively.render = function () {
-    var elem
+  this.check()
+}
 
-    for (var i = inodes.length - 1; i >= 0; --i) {
-      elem = inodes[i]
-
-      if (inView(elem) && elem.classList.contains('progressive--not-loaded')) {
-        loadImage(elem)
-        inodes.splice(i, 1)
-      }
-    }
-
-    this.check()
+progressively.check = function () {
+  if (!inodes.length) {
+    defaults.onLoadComplete()
+    this.drop()
   }
+}
 
-  progressively.check = function () {
-    if (!inodes.length) {
-      defaults.onLoadComplete()
-      this.drop()
-    }
+progressively.drop = function () {
+  if (document.removeEventListener) {
+    window.removeEventListener('scroll', listen)
+  } else {
+    window.detachEvent('onscroll', listen)
   }
+  clearTimeout(poll)
+}
 
-  progressively.drop = function () {
-    if (document.removeEventListener) {
-      root.removeEventListener('scroll', listen)
-    } else {
-      root.detachEvent('onscroll', listen)
-    }
-    clearTimeout(poll)
-  }
-
-  return progressively
-})
+export default progressively
